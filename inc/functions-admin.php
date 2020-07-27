57d0526658ae2fdcdb657eb1811a69c3658345d2
JamesWChan <jwchan1993@gmail.com>
2020-07-27 14:12:57 +0100

    add resource page

---
+++ inc/functions-admin.php
@@ -0,0 +1,72 @@
+<?php
+function stories_resource_metaboxes() {
+    if ( isset( $_GET['post'] ) ) {
+        $post_id = $_GET['post'];
+    } else {
+        if ( isset( $_POST['post_ID'] ) ) {
+            $post_id = $_POST['post_ID'];
+        } else {
+            $post_id = '';
+        }
+    }
+    if ( ! isset( $post_id ) ) {
+        return;
+    }
+    $stories_resource_metabox_array = array(
+        array(
+            'id'       => 'page_options',
+            'title'    => 'Stories resource options',
+            'pages'    => 'page',
+            'context'  => 'normal',
+            'priority' => 'low',
+            'fields'   => array(
+                array(
+                    'name' => 'Stories resource H2',
+                    'desc' => '',
+                    'id'   => 'stories_resource_h2_1',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Stories resource audio link',
+                    'desc' => '',
+                    'id'   => 'stories_resource_audio_link',
+                    'type' => 'textarea',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Stories resource H2',
+                    'desc' => '',
+                    'id'   => 'stories_resource_h2_2',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Stories resource questions',
+                    'desc' => '',
+                    'id'   => 'stories_resource_questions',
+                    'type' => 'textarea',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Stories resource H2',
+                    'desc' => '',
+                    'id'   => 'stories_resource_h2_3',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Stories resource "Do you know"',
+                    'desc' => '',
+                    'id'   => 'stories_resource_do_you_know',
+                    'type' => 'textarea',
+                    'std'  => ''
+                )
+            )
+        )
+    );
+    $template_file      = get_post_meta( $post_id, '_wp_page_template', true );
+    if ( $template_file == 'stories-resource.php' ) {
+        $stories_resource_metabox_array = new CreateMetaBox( $stories_resource_metabox_array[0] );
+    }
+}
---
+++ stories-resource.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * Template Name: Stories Resource Page
+ *
+ */
+get_header(); ?>
+
+<?php get_template_part( 'breadcrumb' ); ?>
+
+    <div id="primary" class="content-area level-one">
+        <div class="container">
+            <div class="row" role="banner">
+                <div class="col-md-12">
+                    <?php
+                    global $post;
+                    $image      = make_path_relative_no_pre_path( get_feature_image_url( $post->ID, 'full-page-width', true ) );
+                    $title      = get_the_title();
+                    get_page_banner( 'level one', $title, $image, '', '', '' );
+                    ?>
+                </div>
+            </div>
+
+            <div class="row">
+                <main role="main">
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <article>
+                                    <div class="row entry-content">
+                                        <div class="col-md-12">
+                                            <h2><?php echo get_post_meta( get_the_ID(), 'stories_resource_h2_1', true ) ?></h2>
+                                            <?php echo get_post_meta( get_the_ID(), 'stories_resource_audio_link', true ) ?>
+                                        </div>
+                                    </div>
+
+                                    <div class="row entry-content">
+                                        <div class="col-md-12">
+                                            <h2><?php echo get_post_meta( get_the_ID(), 'stories_resource_h2_2', true ) ?></h2>
+                                            <div class="activity-container">
+                                                <ul>
+                                                    <?php echo get_post_meta( get_the_ID(), 'stories_resource_questions', true ) ?>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row entry-content">
+                                        <div class="col-md-12">
+                                            <h2><?php echo get_post_meta( get_the_ID(), 'stories_resource_h2_3', true ) ?></h2>
+                                            <?php echo get_post_meta( get_the_ID(), 'stories_resource_do_you_know', true ) ?>
+                                        </div>
+                                    </div>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+
+            </div>
+
+
+        </div>
+    </div>
+
+<?php get_footer(); ?>
